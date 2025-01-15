94b63375898d81b8ab6ccaa28838771fe98baf64
ash <asboy2035@icloud.com>
2025-01-15 18:14:40 +0100

    Initial Commit

---
+++ FakeUI/FakeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeUIApp.swift
+//  FakeUI
+//
+//  Created by ash on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeUITests/FakeUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeUITests.swift
+//  FakeUITests
+//
+//  Created by ash on 1/15/25.
+//
+
+import Testing
+@testable import FakeUI
+
+struct FakeUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
